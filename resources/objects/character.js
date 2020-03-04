b076301964558fbe44887f3adfcc00a88a58214d
Rasmus Eriksson <multi950@gmail.com>
2020-03-04 19:43:49 +0100

    updated the character creation page with headers, text fields and buttons, as well as made the prev and next button working

---
+++ resources/objects/character.js
@@ -0,0 +1,30 @@
+/**
+ * @param {Object} description Description of the character
+ * @param {Object} ancestry
+ * @param {Object} background
+ * @param {Object} character_class
+ */
+function Character(description, ancestry, background, character_class) {
+    this.description = description;
+    this.ancestry = ancestry;
+    this.background = background;
+    this.character_class = character_class;
+}
+/**
+ * @param {String} description Description of the character
+ */
+function Description() {
+
+}
+
+function Ancestry() {
+
+}
+
+function Background() {
+
+}
+
+function character_class() {
+
+}
--- web/javascript/character_creation_tab.js
+++ web/javascript/character_creation_tab.js
@@ -1,6 +1,9 @@
+var tabs = ["introduction", "ancestry", "background", "class", "skill", "overview" ];
+var currentTab;
+
 function openSection(evt, section) {
-    var i, tabcontent, tablinks;
-  
+    var i, tabcontent, tablinks, currentIndex;
+    currentTab = section;
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -17,4 +20,36 @@ function openSection(evt, section) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(section).style.display = "block";
     evt.currentTarget.className += " active";
+
   }
+
+  function  nextSection(){
+      //click the next tab unless it is the last one
+      currentIndex = getCurrentIndex();
+     if(currentIndex === tabContents.length - 1){
+       return;
+     } else{
+       document.getElementsByClassName("tablinks")[currentIndex + 1].click();  
+     }
+    }
+
+    function  previousSection(){
+      currentIndex = getCurrentIndex();
+      //click the next tab unless it is the first one
+     if(currentIndex === 0){
+       return;
+     } else{
+       document.getElementsByClassName("tablinks")[currentIndex - 1].click();  
+     }
+    }
+
+    function getCurrentIndex(){
+            //get current id by comparing the current tab and the tabs on the page
+            tabContents = document.getElementsByClassName("tabcontent");
+            for (i = 0; i < tabContents.length; i++) {
+              if(tabContents[i].id === currentTab){
+                    return i;
+                  }
+            }
+            
+    }
