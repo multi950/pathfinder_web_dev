9188379892b492930c7790db7a7790fd56046a34
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-03-17 20:05:29 +0100

    added password has with salt

--- express/database/remoteRepository.js
+++ express/database/remoteRepository.js
@@ -1,20 +1,21 @@
 "use strict";
 
 const connections = require("./connector");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 //Sign up queries
 const signUp = function(req, res) {
-    var userEmail = req.body.email;
-    connections.query('SELECT email FROM user where email = ?', [userEmail], (err, result, fields) =>{ 
+    var user_email = req.body.email;
+    connections.query('SELECT email FROM user where email = ?', [user_email], (err, result, fields) =>{ 
     if(err) {
         throw err;
     }
-    var userPassword = req.body.password;
+    var user_password = req.body.password;
     if(!result.length){
         console.log("hmm");
-        createUser(userEmail, userPassword);
+        createUser(user_email, user_password);
         res.json({
             success:1,
             message: "account created"
@@ -31,36 +32,64 @@ const signUp = function(req, res) {
 
 
 const createUser = function(email, password){
-    connections.query("INSERT INTO user(email, password) VALUES (?, ?)",[email, password], (err, result)=>{
-        if(err) {
-            throw err;
+    bcrypt.hash(password, 10, (err, hash) => {
+        if(err){
+            return res.status(500).json({
+                error: err
+            });
+        } else {
+            connections.query("INSERT INTO user(email, password) VALUES (?, ?)",[email, hash], (err, result)=>{
+                if(err) {
+                    res.status(500).json({
+                        error: err
+                    });
+                } else {
+                    res.status(201).json({
+                        message: "user created"
+                    });  
+                }
+            });
         }
-    })
+    });
 };
 
 //login queries
 const login = function(req, res) {
-    var email = req.body.email;
-    var password = req.body.password;
+    var user_email = req.body.email;
+    var user_password = req.body.password;
     console.log(req.body);
-    connections.query("SELECT email, password FROM user WHERE email = '"+email+"'", (err, result, fields) =>{
+    connections.query("SELECT email, password FROM user WHERE email = ?",[user_email], (err, result, fields) =>{
         if(err) {
             throw err;
         }
         if(!result.length || result[0] == undefined){
-            res.json({
+            return res.json({
                 success:0,
-                data: "Invalid email or password email"
+                data: "Auth failed"
             });
-        }
-    
-        else if(password===result[0].password){
-            const token = jwt.sign({
-                email: email
-            },
-            process.env.JWT_KEY, {
-                expiresIn: "1h"
+        } 
+        bcrypt.compare(user_password, result[0].password, (err, result) => {
+            if(err){
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            } else if(result) {
+                const token = jwt.sign({
+                    email: email
+                },
+                process.env.JWT_KEY, {
+                    expiresIn: "1h"
+                });
+                return res.status(200).json({
+                    message: "Auth successful",
+                    token: token
+                });
+            }
+            res.status(401).json({
+                message: "Auth failed"
             });
+        }) ;
+        /*if(user_password===result[0].password){
             return res.json({
                 success:1,
                 message: "login successfully",
@@ -71,15 +100,6 @@ const login = function(req, res) {
                 success:0,
                 data: "Invalid email or password password"
             });
-        }
-       /* console.log(result);
-        var row = result[0];
-        var dbPassword = row.password;
-        var dbEmail = row.email;
-        if(dbPassword === password && dbEmail === email){
-            res.send("user verified");
-        } else {
-            res.send("Email or password is incorrect");
         }*/
     });
 };
@@ -99,7 +119,7 @@ const getCharacters = function(req, res) {
 const createCharacter = function(req, res){
     information = req.body.information;
     user_email = req.body.user_email;
-    connections.query("INSERT INTO character(information, user_email) VALUES('"+information+"','"+user_email+"'", (err, result)=>{
+    connections.query("INSERT INTO character(information, user_email) VALUES(?, ?)",[information, user_email] (err, result)=>{
         if(err) {
             throw err;
         }
@@ -112,7 +132,7 @@ const updateCharacter = function(req, res){
     information = req.body.information;
     user_email = req.body.user_email;
     characterid = req.body.characterid;
-    connections.query("UPDATE Character SET JSON = “JSON” WHERE id = "+id+" AND user_email = '"+user_email+"'", (err, result)=>{
+    connections.query("UPDATE Character SET JSON = “JSON” WHERE id = ? AND user_email = ?",[characterid, user_email], (err, result)=>{
         if(err) {
             throw err;
         }
@@ -124,7 +144,7 @@ const updateCharacter = function(req, res){
 const deleteCharacter = function(req, res){
     user_email = req.body.user_email;
     characterid = req.body.characterid;
-    connections.query("DELETE FROM character WHERE id = "+characterid+" AND user_email = '"+user_email+"'", (err, result)=>{
+    connections.query("DELETE FROM character WHERE id = ? AND user_email = ?",[characterid, user_email], (err, result)=>{
         if(err) {
             throw err;
         }
--- express/route/charactersRoute.js
+++ express/route/charactersRoute.js
@@ -20,54 +20,47 @@ router.get("/", checkAuth, (req, res) => {
     repository.getCharacters(req, res);
 });
 
-router.post("/", (req, res) => {
-    res.json({
-        message: "WORKING!"
-    });
-    //repository.createCharacters(req, res);
+router.post("/", checkAuth, (req, res) => {
+    repository.createCharacters(req, res);
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", checkAuth, (req, res) => {
     repository.updateCharacter(req, res);
 });
 
-router.delete("/:id", checkAuth, (req, res) => {
+router.delete("/:id", checkAuth, checkAuth, (req, res) => {
     repository.deleteCharacter(req, res);
 });
 
-router.get("/:id/ancestry", (req, res) => {
-    res.json({
-        success:1,
-        message: "you are the best"
-    });
-    //repository.getAncestries(req, res);
+router.get("/:id/ancestry", checkAuth, (req, res) => {
+    repository.getAncestries(req, res);
 });
 
-router.get("/:id/heritage", (req, res) => {
+router.get("/:id/heritage", checkAuth, (req, res) => {
     repository.getHeritages(req, res);
 });
 
-router.get("/:id/ancestryFeat", (req, res) => {
+router.get("/:id/ancestryFeat", checkAuth, checkAuth, (req, res) => {
     repository.getAncestryFeats(req, res);
 });
 
-router.get("/:id/background", (req, res) => {
+router.get("/:id/background", checkAuth, (req, res) => {
     repository.getBackgrounds(req, res);
 });
 
-router.get("/:id/class", (req, res) => {
+router.get("/:id/class", checkAuth, (req, res) => {
     repository.getClasses(req, res);
 });
 
-router.get("/:id/classoption", (req, res) => {
+router.get("/:id/classoption", checkAuth, (req, res) => {
     repository.getClassOption(req, res);
 });
 
-router.get("/:id/classfeature", (req, res) => {
+router.get("/:id/classfeature", checkAuth, (req, res) => {
     repository.getClassFeatures(req, res);
 });
 
-router.get("/:id/classfeat", (req, res) => {
+router.get("/:id/classfeat", checkAuth, (req, res) => {
     repository.getClassFeats(req, res);
 });
 
