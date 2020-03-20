165de39f26a86fbdfc8c7d09f3cdd570312ac918
kjronning <kjronning@gmail.com>
2020-03-20 13:55:02 +0100

    Class extra information and css change

--- express/index.js
+++ express/index.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 require("dotenv").config();
@@ -18,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname,  "../web/css/")));
 app.use(express.static(path.join(__dirname, "../web/html/")));
 app.use(express.static(path.join(__dirname, "../web/")));
-app.use("/user", routeIndex);
+app.use("/users", routeIndex);
 app.use("/characters", routeCharacters);
 app.listen(port, logStartUpDetailsToConsole);
 
