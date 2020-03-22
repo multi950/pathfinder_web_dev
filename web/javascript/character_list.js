79f02b40736b0777489886e18a6b8b49e645bd82
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-03-22 15:24:28 +0100

    added buttons and onclicks

--- web/javascript/character_list.js
+++ web/javascript/character_list.js
@@ -1,27 +1,45 @@
-
-async function getCharacters(user){
-    const response = await fetch("http://localhost:1337/characters/");
-    const data = await response.json();
-    return data.result;
-}
-
-const email = "Marcus.f.s@hotmail.com";
+const email = getCookie("email", document.cookie);
 
 const characters = getCharacters(email).then(data =>{
     const parent_div = document.getElementById("character_box");
     data.forEach(
     (character) => {
     const information = JSON.parse(character.information);
+    //creating elements
     const div = document.createElement("div");
-    const name_p = document.createElement("p");
-    const class_p = document.createElement("p");
-    const race_p = document.createElement("p");
-    name_p.innerHTML = "name: " +information.name;
-    class_p.innerHTML = "Class: " +information.class;
-    race_p.innerHTML = "race: " +information.race;
-    div.appendChild(name_p);
-    div.appendChild(class_p);
-    div.appendChild(race_p);
+    const character_name_header = document.createElement("h3");
+    const character_p = document.createElement("p");
+    const delete_button = document.createElement("button");
+    const edit_button = document.createElement("button");
+    const view_button = document.createElement("button");
+    //setting on.onclick functions
+    delete_button.onclick = function(){
+        const response = deleteCharacter(character.id);
+        console.log(response);
+        div.remove();
+    };
+    edit_button.onclick = function(){
+        location.href='character_creation.html';
+    };
+    view_button.onclick = function(){
+        
+    };
+
+    //setting innerHTML for buttons
+    delete_button.innerHTML = "Delete";
+    edit_button.innerHTML = "Edit";
+    view_button.innerHTML = "View";
+    //setting innerHTML for p
+    character_p.innerHTML = information.lvl + " | " + information.race + " | " + information.class + "";
+    //setting innerHTML for header
+    character_name_header.innerHTML = information.name;
+
+    //Appending them to parents
+    div.appendChild(character_name_header);
+    div.appendChild(character_p);
+    div.appendChild(delete_button);
+    div.appendChild(edit_button);
+    div.appendChild(view_button);
     parent_div.appendChild(div);
 } 
 );
