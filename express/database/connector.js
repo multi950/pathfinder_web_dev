7b38fac639edc1d4a8c30d5a4dc5dd77e018d6e4
Marcus Sørensen <marcus.f.s@hotmail.com>
2020-03-23 01:31:51 +0100

    upped the css on character list

--- express/database/connector.js
+++ express/database/connector.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql');
-/** 
+
 const databaseCredentials = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.MYSQL_DB
 };
-*/
 
- const databaseCredentials = {
+
+ /*const databaseCredentials = {
   host: 'den1.mysql2.gear.host',
   user: 'pathfindercc',
   password: 'Xs8PZ?2~8V7m',
   database: 'pathfindercc'
-};
+};*/
  
 const connection = mysql.createConnection(databaseCredentials);
 
